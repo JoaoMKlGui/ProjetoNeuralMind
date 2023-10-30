7162383d0b5d656b0280e48723c064d93e92e8f5
JoaoMKlGui <j260542@dac.unicamp.br>
2023-10-30 16:36:40 -0300

    ultimo upload

--- main.py
+++ main.py
@@ -2,6 +2,6 @@ import retriever
 import pipeline
 import conversation
 
-path = r'C:\Projeto NeuralMind\Procuradoria Geral - Normas - Vestibular 2024.pdf'
-open_ai_key = "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"
+path = r'insira o path'
+open_ai_key = "insira a chave da api aqui"
 conversation.run_conversation(open_ai_key, path)
